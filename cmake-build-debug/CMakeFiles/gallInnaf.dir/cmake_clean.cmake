e7633bf0dd6999062f135c3fe0a0451072bcb416
gallochri <chri@gallochri.com>
2016-12-13 22:34:23 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/gallInnaf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gallInnaf.dir/src/main.cpp.o"
+  "gallInnaf.pdb"
+  "gallInnaf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gallInnaf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
