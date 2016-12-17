52422173ddc32558d0795ce12ac1f80b3fd22efb
gallochri <chri@gallochri.com>
2016-12-17 14:14:23 +0100

    Ubidots first commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <SFE_MicroOLED.h>
-#include <BlynkSimpleEsp8266.h>
+#include "../lib/ubidots-esp8266/UbidotsESP8266.h"
+#include <SoftwareSerial.h>
 
 #include "user_config.h"
 
@@ -11,7 +12,7 @@
 #define DC_JUMPER 0
 
 MicroOLED oled(PIN_RESET, DC_JUMPER);
-WidgetLED led1(V0);
+Ubidots client(TOKEN);
 
 void printTitle(String title, int font) {
     int middleX = oled.getLCDWidth() / 2;
@@ -19,7 +20,7 @@ void printTitle(String title, int font) {
     oled.clear(PAGE);
     oled.setFontType(font);
     // Try to set the cursor in the middle of the screen
-    oled.setCursor(0,middleY - (oled.getFontWidth() / 2));
+    oled.setCursor(0, middleY - (oled.getFontWidth() / 2));
     // Print the title:
     oled.print(title);
     oled.display();
@@ -28,25 +29,22 @@ void printTitle(String title, int font) {
 }
 
 void setup() {
+    client.wifiConnection(ssid,pass);
     Serial.begin(115200);
     oled.begin();
     oled.clear(ALL);
     oled.display();
-    Blynk.begin(auth,ssid,pass,domain,port);
 }
 
 void loop() {
-    Blynk.run();
     int sensorValue = analogRead(A0);
     Serial.print(sensorValue);
     int sensorON = digitalRead(D5);
     Serial.print(" Digital output:");
     Serial.println(sensorON);
-    if (sensorON == 0){
-        led1.on();
-    } else {
-        led1.off();
-    }
     printTitle("Value:" + String(sensorValue), 0);
+    client.add(ID,sensorValue);
+    client.sendAll();
     delay(100);
 }
+
